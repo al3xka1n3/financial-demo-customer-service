5328ba4800791fb2da878f99bb9752af3f4ccd4d
al3xka1n3 <al3xka1n3@gmail.com>
2021-04-08 21:40:48 +0300

    updated build.gradle.kts: added Spring Boot and Spring MVC, set Java version as 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,5 +10,12 @@ repositories {
 }
 
 dependencies {
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.3.9.RELEASE"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
